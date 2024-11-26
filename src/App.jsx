76aa9fc001db3dc83fe9368e25d890c5e07ad6fe
fridfn <faridfathonin@gmail.com>
2024-11-26 15:44:22 +0700

    menambahkan fitur pelajari di setiap list dan membalikan warna menjadi warna fossil agar lebih menyatu

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
    setTimeout(() => {
     setLoading(false)
-   }, 10000);
+   }, 15000);
   }, []);
   
   return (
--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import iconDino from '/src/assets/dino.svg';
 
 const List = () => {
   const [dinosaurus, setDinosaurus] = useState([]);
@@ -7,7 +8,7 @@ const List = () => {
     <div className="list">
      <div className="wrapper width-full" id="background-list">
        <div className="box-image">
-         <img className="icon-image" src="./dinosaurus.jpg"/>
+         <img className="icon-image" src={iconDino}/>
        </div>
        <p className="name-list small">Dinosaurus</p>
        <div className="cta">
@@ -16,9 +17,16 @@ const List = () => {
       </div>
       <div className="descriptions-box">
         <p className="title litle">Zaman Neolitikum</p>
-        <div className="wrapping" style={{boxShadow: 'inset 0px 3px 5px 4px var(--primary-cards)'}}>
+        <div className="wrapping" style={{boxShadow: 'inset 0px 3px 5px 4px var(--primary-cards)', borderRadius: '5px'}}>
          <p className="description tiny marquee">commodo id in officia adipisicing nostrud et ad qui elit excepteur magna in mollit eiusmod Lorem ad qui nisi nulla ut aute exercitation proident incididunt anim aliquip aliquip sunt anim Lorem exercitation eu laboris ea commodo do ex officia excepteur</p>
         </div>
+      </div>
+      <div className="wrapper-flex" style={{width: '100%'}}>
+        <div className="button-list wrapper-flex">
+         <button className="button tiny-bold">PELAJARI</button>
+         <ion-icon class="icon small" id="paw" name="paw"></ion-icon>
+        </div>
+         <ion-icon class="icon big" id="paw" name="information"></ion-icon>
       </div>
     </div>
    );
