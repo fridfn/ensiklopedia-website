12270eec6bdf7ad9b68fc5869377fb6f8e7a4e47
fridfn <faridfathonin@gmail.com>
2024-12-07 19:32:33 +0700

    menambahkan plugin untuk PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
 import envCompatible from 'vite-plugin-env-compatible';
 
 export default defineConfig({
-  plugins: [react(), envCompatible()],
+  plugins: [
+  react(),
+  envCompatible(),
+  VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Ensiklopedia Apps',
+        short_name: 'Ensiklopedia',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#05210a',
+        theme_color: '#0b2810',
+        icons: [
+          {
+          "src": "/src/assets/dinosaurus.jpg",
+          "sizes": "192x192",
+          "type": "image/png"
+         },
+         {
+          "src": "/src/assets/dinosaurus.jpg",
+          "sizes": "512x512",
+          "type": "image/png"
+         }
+        ]
+      }
+    })],
   resolve: {
     alias: {
       '@': '/src',
