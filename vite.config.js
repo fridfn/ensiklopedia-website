1e35d01e182c3f45677891338bcf16d4a9cb3b51
fridfn <faridfathonin@gmail.com>
2024-12-20 19:10:15 +0700

    memperbarui file sebelunnya karna error gak jelas dan mengintegrasikan ulang ke github

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,13 @@
 import path from 'path';
-import sass from 'vite-plugin-sass'
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from 'vite-plugin-pwa';
 import envCompatible from 'vite-plugin-env-compatible';
 
 export default defineConfig({
   plugins: [
-  react(),
-  sass(),
-  envCompatible(),
-  VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Ensiklopedia Apps',
-        short_name: 'Ensiklopedia',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#05210a',
-        theme_color: '#0b2810',
-        icons: [
-          {
-          "src": "/src/assets/dinosaurus.jpg",
-          "sizes": "192x192",
-          "type": "image/png"
-         },
-         {
-          "src": "/src/assets/dinosaurus.jpg",
-          "sizes": "512x512",
-          "type": "image/png"
-         }
-        ]
-      }
-    })],
+   react(),
+   envCompatible()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
